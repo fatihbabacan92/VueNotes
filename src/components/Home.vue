208b8bda60b329ffd2c1ae4f356540dfe9543491
Fatih <fatihb@live.be>
2020-01-22 19:58:33 +0100

    Added Home Component

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to VueNotes" />
+    <button type="button" class="btn btn-primary">Go to Notes</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
