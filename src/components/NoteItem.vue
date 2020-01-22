a2673b5aee22c41c62a710ed1c60d0f6988d088b
Fatih <fatihb@live.be>
2020-01-22 20:57:06 +0100

    Created NoteItem

---
+++ src/components/NoteItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card border-secondary mb-3" style="max-width: 20rem;">
+    <div class="card-header">{note.title}</div>
+    <div class="card-body">
+      <p class="card-text">{note.text}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'NoteItem',
+    props: ['note'],
+    methods: {
+        delete() {
+            
+        }
+    }
+};
+</script>
+
+<style>
+</style>
