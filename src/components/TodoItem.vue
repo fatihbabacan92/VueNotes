4cbac16f4c7df51d72e18d17065fec7d767e1412
Fatih <fatihb@live.be>
2020-01-23 18:28:20 +0100

    Added TodoItem Component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="todo-item-card m-2 p-1">
+      <p class="lead d-inline m-2 text-dark">{{todo.text}}</p>
+      <button @click="$emit('deleteTodo')" class="btn btn-danger btn-sm d-inline m-2 float-right">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'TodoItem',
+    props: ["todo"]
+}
+</script>
+
+<style>
+.todo-item-card {
+    background-color: aliceblue;
+    border-radius: 35px;
+    min-height: 3rem;
+}
+</style>
