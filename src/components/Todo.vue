81b5d8312292f4a94ab5a3ed8fb3ea955195bfa9
Fatih <fatihb@live.be>
2020-01-23 18:11:52 +0100

    Added Todo Component with All Functionalities

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,13 +1,58 @@
 <template>
-  <h1>Coming Soon...</h1>
+  <div class="todo-card mx-auto shadow-lg">
+    <input v-model="newTodo.title" type="text" placeholder="Write down your todo..." class="form-control m-2 w-50" />
+    <button @click="addTodo" class="btn btn-primary shadow-lg m-2">Add Todo</button>
+    <TodoItem @deleteTodo="deleteTodo(todo)" v-for="t in todos" v-bind:note="t" v-bind:key="t.id"/>
+    <button class="btn btn-warning btn-sm d-block mx-auto">Clear All</button>
+  </div>
 </template>
 
 <script>
+import TodoItem from "@/components/TodoItem.vue"
 export default {
-    name: 'Todo'
+    name: 'Todo',
+    components: {
+      TodoItem
+    },
+    data(){
+      return {
+        todos: [],
+        newTodo: []
+      }
+    },
+    mounted() {
+      if (localStorage.getItem("todos")) {
+        localStorage.clear()
+      }
+    },
+    methods: {
+      addTodo() {
+        if (!this.newTodo) {
+          return;
+        }
+        this.newTodo.id = this.todos.length
+        this.todos.push(this.newTodo)
+        this.newTodo = []
+        this.saveTodos()
+      },
+      saveTodos() {
+        const parsed = JSON.stringify(this.todos)
+        localStorage.setItem("todos", parsed)
+      },
+      deleteTodo(todo) {
+        this.todos.splice(this.todos.indexOf(todo), 1)
+        this.saveTodos()
+      }
+    }
 }
 </script>
 
 <style>
-
+.todo-card {
+  width: 75vw;
+  height: 50vh;;
+  background-color: blueviolet;
+  color: white;
+  border-radius: 35px;
+}
 </style>
